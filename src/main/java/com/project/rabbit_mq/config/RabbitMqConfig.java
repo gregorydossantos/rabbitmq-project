021bd07d95189c3a36bfb1cc03fd56007ceb2f0a
gregory <gregescribano@gmail.com>
2024-07-08 21:19:53 -0300

    Add configs between spring and rabbit-mq and postgres

---
+++ src/main/java/com/project/rabbit_mq/config/RabbitMqConfig.java
@@ -0,0 +1,9 @@
+package com.project.rabbit_mq.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    public static final String CREATE_QUEUE = "create-queue";
+}
